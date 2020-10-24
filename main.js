d5596a50a79cc9045e163ffabc78e92592780aab
Bidesh15-8550 <bideshbiswas@ymail.com>
2020-10-24 19:27:15 +0600

    let

--- main.js
+++ main.js
@@ -3,8 +3,13 @@ console.log('Hello World');
 console.error('This is an error');
 console.warn('This is a warning');
 
-//let(reassign value),const
+//let(reassign value or changeable),const(cant be changed)
 
 let age=30;
-
+age=31;
 console.log(age);
+
+let score;
+
+score = 10;
+console.log(score);
