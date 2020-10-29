d726379799c0e5657cb533c9f06e340ff033a1fc
Bidesh15-8550 <bideshbiswas@ymail.com>
2020-10-29 19:28:13 +0600

    prototype function

--- main.js
+++ main.js
@@ -210,12 +210,13 @@ function Person(firstName, lastName,dob){
     this.firstName= firstName;
     this.lastName=lastName;
     this.dob=new Date(dob);
-    this.getBirthYear=function(){
-        return this.dob.getFullYear();
-    }
-    this.getFullName=function(){
-        return `${this.firstName} ${this.lastName}`;
-    }
+}
+
+Person.prototype.getBirthYear = function(){
+    return this.dob.getFullYear();
+}
+Person.prototype.getFullName = function(){
+    return `${this.firstName} ${this.lastName}`;
 }
 
 //itstantiate object
@@ -228,3 +229,5 @@ console.log(person1);
 console.log(person2.dob.getFullYear());
 console.log(person1.getBirthYear());
 console.log(person1.getFullName());
+
+
